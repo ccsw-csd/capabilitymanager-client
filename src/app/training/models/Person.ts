a237c53b0d265774db3508b2340dcb4cc0fa4532
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2024-04-11 12:16:58 +0200

    Actualizada interfaz Person y listado según el objeto del back

--- src/app/training/models/Person.ts
+++ src/app/training/models/Person.ts
@@ -1,22 +1,22 @@
 export interface Person {
-    id: number;
-    saga: string;
-    nombre: string;
-    apellidos: string;
-    practica: string;
-    grado: string;
-    categoria: string;
-    perfil_Tecnico: string;
-    fecha_Incorporacion: Date | null;
-    asignacion: number;
-    status: string;
-    cliente_ctual: string | null;
-    fecha_Inicio_asignacion: Date | null;
-    fecha_Fin_signacion: Date | null;
-    fecha_Disponibilidad: Date | null;
-    posicion_Proyecto_Futuro: string | null;
-    colaboraciones: string; 
-    proyecto_anterior: string | null;
-    ggid: string;
-    meses_Bench: string;
-  }
+  id: number;
+  saga: string;
+  nombre: string;
+  apellidos: string;
+  practica: string;
+  grado: string;
+  categoria: string;
+  perfilTecnico: string;
+  fechaIncorporacion: Date | null;
+  asignacion: number;
+  status: string;
+  clienteActual: string | null;
+  fechaInicioAsignacion: Date | null;
+  fechaFinAsignacion: Date | null;
+  fechaDisponibilidad: Date | null;
+  posicionProyectoFuturo: string | null;
+  colaboraciones: string;
+  proyectoAnterior: string | null;
+  ggid: string;
+  mesesBench: string;
+}
