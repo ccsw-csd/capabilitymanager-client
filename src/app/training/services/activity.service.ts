d58e3c72367dc99c1c916e6e0f545da204cb1785
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2024-05-23 14:54:10 +0200

    Creado listado

--- src/app/training/services/activity.service.ts
+++ src/app/training/services/activity.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Activity } from '../models/Activity';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ActivityService {
+  private baseUrl = '/activity';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Activity[]> {
+    return this.http.get<Activity[]>(`${this.baseUrl}/all`);
+  }
+
+  findBySaga(saga: string): Observable<Activity[]> {
+    return this.http.get<Activity[]>(`${this.baseUrl}/${saga}`);
+  }
 }
--- src/app/training/views/activity-list/activity-list.component.ts
+++ src/app/training/views/activity-list/activity-list.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivityService } from '../../services/activity.service';
+import { Activity } from '../../models/Activity';
 
 @Component({
   selector: 'app-activity-list',
   templateUrl: './activity-list.component.html',
   styleUrls: ['./activity-list.component.scss'],
 })
-export class ActivityListComponent {}
+export class ActivityListComponent implements OnInit {
+  activities: Activity[] = [];
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit(): void {
+    this.loadActivities();
+  }
+
+  loadActivities(): void {
+    this.activityService.findAll().subscribe(
+      (data) => {
+        this.activities = data;
+      },
+      (error) => {
+        console.error('Error al cargar las actividades', error);
+      }
+    );
+  }
+}
