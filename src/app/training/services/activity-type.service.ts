5fe46a6fb56855f06a42c9f56e1c6dcf9450cbaf
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2024-05-27 11:01:20 +0200

    Creado service para ActivityType

---
+++ src/app/training/services/activity-type.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivityType } from '../models/ActivityType';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ActivityTypeService {
+  private baseUrl: string = environment.server;
+
+  constructor(private http: HttpClient) {}
+
+  getAllPersons(): Observable<ActivityType[]> {
+    return this.http.get<ActivityType[]>(`${this.baseUrl}/activity-types`);
+  }
+
+  getActivityTypeById(id: number): Observable<ActivityType[]> {
+    return this.http.get<ActivityType[]>(
+      `${this.baseUrl}/activity-types/${id}`
+    );
+  }
+}
